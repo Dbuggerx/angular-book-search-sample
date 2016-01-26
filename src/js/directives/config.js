a4e7b9c0e5d27a3f85c4c60d3d1e461eae12768e
Danilo Cestari <dcestari@localhost.localdomain>
2016-01-26 01:27:30 -0200

    Project page

--- src/js/directives/config.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-import refillsDropdown from './refills-dropdown';
-import bookCard from './book-card';
-
-export default function () {
-  angular.module('BooksChallenge')
-    .directive('refillsDropdown', refillsDropdown)
-    .directive('bookCard', bookCard);
-}
